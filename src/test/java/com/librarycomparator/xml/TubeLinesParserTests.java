1e6f83965b8d6920635456e88fb13d3c2ee9e7cc
Wing Hym Liu <Presenter@fionas-mbp.home>
2014-08-09 10:19:45 +0100

    Unit Tests Refactor
    SRP - Each test should test one thing and test one thing well. Makes it
    easier to figure out what went wrong later.

--- src/test/java/com/librarycomparator/xml/TubeLinesParserTests.java
+++ src/test/java/com/librarycomparator/xml/TubeLinesParserTests.java
@@ -1,8 +1,11 @@
 package com.librarycomparator.xml;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
@@ -11,28 +14,47 @@ import static org.junit.Assert.*;
 
 public class TubeLinesParserTests {
 
-	@Test
-	public void GetListOfTubeLinesTest() throws Exception {
-		
+	private TubeLines _tubeLines;
+	
+	@Before
+	public void setUp() throws Exception {
 		Serializer serializer = new Persister();
-		File source = new File(ClassLoader.getSystemResource("TubeLines.xml").toURI());
+		File source = new File(ClassLoader.getSystemResource("TubeLines.xml")
+				.toURI());
+		_tubeLines = serializer.read(TubeLines.class, source);
+	}
 
-		TubeLines tubeLines = serializer.read(TubeLines.class, source);
-		
-		assertNotNull(tubeLines);
-		assertFalse(tubeLines.getTubeLines().isEmpty());
-		
-		TubeLine tubeLine = tubeLines.getTubeLines().get(0);
-		
+	@After
+	public void tearDown() {
+		_tubeLines = null;
+	}
+
+	@Test
+	public void GetListOfTubeLinesIsNotEmptyTest() {
+
+		assertNotNull(_tubeLines);
+		assertFalse(_tubeLines.getTubeLines().isEmpty());
+
+	}
+
+	@Test
+	public void GetWaterlooAndCityLineTest() {
+		TubeLine tubeLine = _tubeLines.getTubeLines().get(0);
+
 		assertTrue(tubeLine.getId().equalsIgnoreCase("12"));
 		assertTrue(tubeLine.getName().equalsIgnoreCase("waterloo and city"));
-		
 		assertFalse(tubeLine.getStations().isEmpty());
-		
-		List<Station> stations = tubeLine.getStations(); 
+
+	}
+
+	@Test
+	public void GetWaterlooAndCityLineStationsTest() {
+		TubeLine tubeLine = _tubeLines.getTubeLines().get(0);
+
+		List<Station> stations = tubeLine.getStations();
 		assertTrue(stations.get(0).getId().equalsIgnoreCase("252"));
 		assertTrue(stations.get(0).getName().equalsIgnoreCase("waterloo"));
-		
+
 		assertTrue(stations.get(1).getId().equalsIgnoreCase("12"));
 		assertTrue(stations.get(1).getName().equalsIgnoreCase("bank"));
 	}
