17f54249c1337c5d4c468e84d2282a0960c06f90
Wing Hym Liu <Presenter@fionas-MacBook-Pro.local>
2014-08-28 20:32:20 +0100

    Stations and lines

--- src/main/java/com/travelalerter/domain/stations/Line.java
+++ src/main/java/com/travelalerter/domain/stations/Line.java
@@ -5,48 +5,31 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.simpleframework.xml.Attribute;
-import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 
 @Root
-public class Station {
-
+public class Line {
 	@Attribute(name="ID")
 	private String id;
 
-	@Attribute(name="Name")
-	private String name;
-
-	@ElementList(name="Lines")
-	private Lines lines;
-
-	public Station(@Attribute(name="ID") String id, @Attribute(name="Name") String name) {
+	public Line(@Attribute(name="ID") String id) {
 		this.id = id;
-		this.name = name;
 	}
 
 	public String getId() {
 		return id;
 	}
 
-	public String getName() {
-		return name;
-	}
-	
-	public Lines getLines(){
-		return lines;
-	}
-	
 	@Override
 	public int hashCode() {
 		return HashCodeBuilder.reflectionHashCode(this);
 	}
-	
+
 	@Override
 	public boolean equals(Object other) {
 		return EqualsBuilder.reflectionEquals(this, other);
 	}
-	
+
 	@Override
 	public String toString() {
 		return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
--- src/main/java/com/travelalerter/domain/stations/Lines.java
+++ src/main/java/com/travelalerter/domain/stations/Lines.java
@@ -1,52 +1,40 @@
 package com.travelalerter.domain.stations;
 
+import java.util.List;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 
-@Root
-public class Station {
+import com.travelalerter.domain.lines.Line;
 
-	@Attribute(name="ID")
-	private String id;
+@Root(name = "Lines")
+public class Lines {
+	@ElementList(name="Lines", entry = "Line", inline = true)
+	private List<Line> lines;
 
-	@Attribute(name="Name")
-	private String name;
-
-	@ElementList(name="Lines")
-	private Lines lines;
-
-	public Station(@Attribute(name="ID") String id, @Attribute(name="Name") String name) {
-		this.id = id;
-		this.name = name;
+	public Lines(@ElementList(name="Lines", entry = "Line", inline = true) List<Line> lines) {
+		this.lines = lines;
 	}
 
-	public String getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-	
-	public Lines getLines(){
+	public List<Line> getTubeLines() {
 		return lines;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return HashCodeBuilder.reflectionHashCode(this);
 	}
-	
+
 	@Override
 	public boolean equals(Object other) {
 		return EqualsBuilder.reflectionEquals(this, other);
 	}
-	
+
+
 	@Override
 	public String toString() {
 		return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
